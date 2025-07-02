69b4708db630ff300a670ea599a9f36df8e6b9ad
SatyaCompose <satya@compose.co.in>
2025-07-02 15:41:10 +0530

    chore: remove debug logs from user creation and customer mapping functions

--- src/utils/customerCreateMapper.ts
+++ src/utils/customerCreateMapper.ts
@@ -19,11 +19,8 @@ export const prepareCTPayload = (customerData: AccountRegisterBody, customFields
     const uniqueStores = Array.from(
         new Map(storesDraft.map(store => [store.key, store])).values()
     );
-    console.log("CUSTOMER DATA", accountRegisterBody);
     const sitekeys = [...customFieldsData.siteKey ?? '', `${organizationName}|${currentSiteKey}`];
-    console.log("SITEKEYS", sitekeys)
     const uniqueSiteKeys = Array.from(new Set(sitekeys));
-    console.log("UNIQUE SITE KEYS", uniqueSiteKeys);
     const customer = {
         email: accountRegisterBody?.email,
         password: accountRegisterBody?.password,
